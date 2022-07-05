47f2b7110b422bcd04fb8cf6fbf5e591f658ec76
Alex Di Guida <alexandre.diguida@gmail.com>
2022-07-05 19:40:20 +0100

    Shader cleaning

--- js/Experience/shaders/visualizer/fragment.glsl
+++ js/Experience/shaders/visualizer/fragment.glsl
@@ -6,19 +6,6 @@ varying float vZ;
 varying vec3 vUv;
 
 void main() {
-  // gl_FragColor = vec4(mix(u_black, u_white, vUv.x), 1.0);
-
-  // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-  // if (vUv.x < 0.0) {
-  //   gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
-  // } else {
-  //   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-  // }
-  // gl_FragColor = vec4(abs(sin(uTime * .001)), 0.0, 0.0, 1.0);
-  gl_FragColor = vec4((32.0 - abs(vX)) / 32.0, (32.0 - abs(vY)) / 32.0, (abs(vX + vY) / 2.0) / 32.0, 1.0);
+  float offset = 24.0;
+  gl_FragColor = vec4((offset - abs(vX)) / offset, (offset - abs(vY)) / offset, (abs(vX + vY) / 1.65) / offset, 1.0);
 }
-
-
-// void main() {
-//   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-// }
--- js/Experience/shaders/visualizer/vertex.glsl
+++ js/Experience/shaders/visualizer/vertex.glsl
@@ -17,12 +17,7 @@ void main() {
     float xMultiplier = (32.0 - x) / 8.0;
     float yMultiplier = (32.0 - y) / 8.0;
 
-    //float z = position.z;
-    //float z = abs(position.x) + abs(position.y);
-    //float z = sin(abs(position.x) + abs(position.y));
-    //float z = sin(abs(position.x) + abs(position.y) + uTime * .005);
     float z = sin(uDataArray[int(floorX)] / 50.0 + uDataArray[int(floorY)] / 50.0) * uAmplitude;
-    // float z = (uDataArray[int(floorX)] / 50.0 + uDataArray[int(floorY)] / 50.0) * 2.0;
 
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position.x, position.y, z, 1.0);
 
@@ -31,13 +26,3 @@ void main() {
     vY = y;
     vZ = z;
 }
-
-// uniform float uTime;
-
-// void main() {
-//     float z = sin(abs(position.x) + abs(position.y) + uTime);
-
-//     vec3 newPosition = vec3(position.xy, z);
-
-//     gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
-// }
